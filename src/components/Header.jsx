43ab7128a17b6802535132b30b9b3c0951d385d9
Takuya Matsuyama <hi@craftz.dog>
2023-08-16 14:20:22 +0900

    fix(logo): use Inkdrop logo and update description

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -69,8 +69,12 @@ export const Header = forwardRef(function Header({ className }, ref) {
         <nav className="hidden md:block">
           <ul role="list" className="flex items-center gap-8">
             <TopLevelNavItem href="/">API</TopLevelNavItem>
-            <TopLevelNavItem href="#">Documentation</TopLevelNavItem>
-            <TopLevelNavItem href="#">Support</TopLevelNavItem>
+            <TopLevelNavItem href="https://docs.inkdrop.app/">
+              Documentation
+            </TopLevelNavItem>
+            <TopLevelNavItem href="https://forum.inkdrop.app/">
+              Support
+            </TopLevelNavItem>
           </ul>
         </nav>
         <div className="hidden md:block md:h-5 md:w-px md:bg-zinc-900/10 md:dark:bg-white/15" />
