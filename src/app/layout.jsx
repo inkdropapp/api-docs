bdeefc2721188fbd0abd0734c47470ab24fc5b95
Takuya Matsuyama <hi@craftz.dog>
2023-12-08 10:37:53 +0900

    feat(analytics): add fathom

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@ import glob from 'fast-glob'
 
 import { Providers } from '@/app/providers'
 import { Layout } from '@/components/Layout'
+import Fathom from '../components/Fathom'
 
 import '@/styles/tailwind.css'
 
@@ -18,13 +19,14 @@ export default async function RootLayout({ children }) {
     pages.map(async (filename) => [
       '/' + filename.replace(/(^|\/)page\.mdx$/, ''),
       (await import(`./${filename}`)).sections,
-    ])
+    ]),
   )
   allSections = Object.fromEntries(allSections)
 
   return (
     <html lang="en" className="h-full" suppressHydrationWarning>
       <body className="flex min-h-full bg-white antialiased dark:bg-zinc-900">
+        <Fathom />
         <Providers>
           <div className="w-full">
             <Layout allSections={allSections}>{children}</Layout>
