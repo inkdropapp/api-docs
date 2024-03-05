276beb17b38022439f91e8a971291d4d39ad0bf6
Takuya Matsuyama <hi@craftz.dog>
2024-03-05 09:31:55 +0900

    fix(guides): update CSON config examples to JSON

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,11 @@
 /** @type {import('prettier').Options} */
 module.exports = {
+  arrowParens: 'avoid',
   singleQuote: true,
+  bracketSpacing: true,
+  endOfLine: 'lf',
   semi: false,
+  tabWidth: 2,
+  trailingComma: 'none',
   plugins: ['prettier-plugin-tailwindcss'],
 }
